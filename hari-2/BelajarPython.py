fe5600f75232e3bb8bef8d24d946f6b5454737aa
Muhammad Sayuti <sayutizxc@gmail.com>
2021-02-28 12:49:27 +0800

    Input dari console menggunakan python

--- hari-2/BelajarPython.py
+++ hari-2/BelajarPython.py
@@ -2,3 +2,8 @@
 nama = "sayuti";
 umur = 19;
 print(f"Nama saya {nama}, umur saya {umur}")
+
+# User input
+nama = input("Masukkan nama anda : ")
+umur = input("Masukkan umur anda : ")
+print(f"Nama saya {nama}, umur saya {umur}")
